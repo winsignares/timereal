79566bee0fea16e13699eb4b4ca0c99c80071ef2
= <=>
2023-11-30 09:32:10 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message:', msg)
+    socketio.emit('message', msg)
+
+@socketio.on('mensaje')
+def handle_message2(msg):
+    print('Emitiendo:', msg)
+    socketio.emit('mensaje', msg)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
